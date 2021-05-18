7827fa8e462700968f3f0421e856b7e2faf48fd6
oscarkhima <boukhimaoscar@gmail.com>
2021-05-18 11:19:35 +0200

    Componentes creados

--- FRONT/angular-tfg/src/app/app.module.ts
+++ FRONT/angular-tfg/src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { WebPageComponent } from './Componentes/web-page/web-page.component';
+import { NavegacionComponent } from './Componentes/web-page/navegacion/navegacion.component';
+import { ClientIndexComponent } from './Componentes/client-index/client-index.component';
+import { LandingPageComponent } from './Componentes/web-page/landing-page/landing-page.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WebPageComponent,
+    NavegacionComponent,
+    ClientIndexComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule
