7ea0555297c17a81142df61390d16d6bf8594a97
Alvaro <alvaromoreiro3@gmail.com>
2021-05-25 23:32:58 +0200

    Arreglada la cagada de ocal

--- FRONT/angular-tfg/src/app/app.module.ts
+++ FRONT/angular-tfg/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { WebPageComponent } from './Componentes/web-page/web-page.component';
 import { NavegacionComponent } from './Componentes/web-page/navegacion/navegacion.component';
 import { ClientIndexComponent } from './Componentes/client-index/client-index.component';
 import { LandingPageComponent } from './Componentes/web-page/landing-page/landing-page.component';
-import { SeccionUnoComponent } from './web-page/seccion-uno/seccion-uno.component';
+import { SeccionUnoComponent } from './Componentes/web-page/seccion-uno/seccion-uno.component';
 @NgModule({
   declarations: [
     AppComponent,
