2ca7980732ab6a31c6eb3c2b05644fbedd5ba66d
Alvaro <alvaromoreiro3@gmail.com>
2021-06-01 01:42:28 +0200

    Login Creado

--- FRONT/angular-tfg/src/app/Componentes/web-page/registro/registro.component.ts
+++ FRONT/angular-tfg/src/app/Componentes/web-page/registro/registro.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.scss']
+  
 })
 export class RegistroComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    AOS.init()
   }
 
 }
--- FRONT/angular-tfg/src/app/app-routing.module.ts
+++ FRONT/angular-tfg/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistroComponent } from './Componentes/web-page/registro/registro.component';
 import { WebPageComponent } from './Componentes/web-page/web-page.component';
+import { IniciarSesionComponent } from './Componentes/web-page/iniciar-sesion/iniciar-sesion.component';
 
 const routes: Routes = [
   { path: '', component: WebPageComponent },
   { path: 'register', component: RegistroComponent },
+  { path: 'login', component: IniciarSesionComponent },
 ];
 
 @NgModule({
